52debe167eaa466a3e81f444a68cdb969859c475
G00405014 <G00405014@atu.ie>
2022-10-28 13:16:36 +0100

    Student app 3 students

--- src/main/java/ie/atu/week5/StudentApp.java
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -19,6 +19,33 @@ public class StudentApp {
         System.out.println("Please enter Name of course Studying");
         String course = input.nextLine();
         System.out.println("You entered" + course);
+
+        System.out.println("Please enter Student Name");
+        String name = input.nextLine();
+        System.out.println("You entered" + name);
+
+        //Asking student for email
+        System.out.println("Please enter Student email");
+        String email = input.nextLine();
+        System.out.println("You entered" + email);
+
+        //Asking Student for course
+        System.out.println("Please enter Name of course Studying");
+        String course = input.nextLine();
+        System.out.println("You entered" + course);
+        System.out.println("Please enter Student Name");
+        String name = input.nextLine();
+        System.out.println("You entered" + name);
+
+        //Asking student for email
+        System.out.println("Please enter Student email");
+        String email = input.nextLine();
+        System.out.println("You entered" + email);
+
+        //Asking Student for course
+        System.out.println("Please enter Name of course Studying");
+        String course = input.nextLine();
+        System.out.println("You entered" + course);
     }
 
 }
