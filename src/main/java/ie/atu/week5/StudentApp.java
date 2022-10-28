d55024764775e21b20d49eadcf3ca006d04c77a9
G00405014 <G00405014@atu.ie>
2022-10-28 13:03:59 +0100

    Student app

---
+++ src/main/java/ie/atu/week5/StudentApp.java
@@ -0,0 +1,24 @@
+package ie.atu.week5;
+import java.util.Scanner;
+
+public class StudentApp {
+    public static void main(String[] args) {
+
+        //Asking student for name
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter Student Name");
+        String name = input.nextLine();
+        System.out.println("You entered" + name);
+
+        //Asking student for email
+        System.out.println("Please enter Student email");
+        String email = input.nextLine();
+        System.out.println("You entered" + email);
+
+        //Asking Student for course
+        System.out.println("Please enter Name of course Studying");
+        String course = input.nextLine();
+        System.out.println("You entered" + course);
+    }
+
+}
